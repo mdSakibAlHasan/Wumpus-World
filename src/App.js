5d6857cc069f1a0a80d1d0d88478b94e0a4be14c
hasib1224 <hasib1224>
2023-09-15 03:48:50 +0600

    some trash files are deleted

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Environment from './Environment/Environment';
+
+// import Homepage from './Pages/homePage';
+import ViewBoard from './Board/board.js'
+import { Routes, Route} from "react-router-dom";
+// import Board from './Board/board';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        {/* <Route path="/" element={<Homepage />} /> */}
+        <Route path="/" element={<Environment />} />
+      </Routes>
     </div>
   );
 }
